cae418b9bcee9f5e6be5fccb0ca9d04f8f457a41
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-20 01:47:34 +0200

    Initial Commit

---
+++ iSplit/iSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSplitApp.swift
+//  iSplit
+//
+//  Created by M. A. Alim Mukul on 20.09.22.
+//
+
+import SwiftUI
+
+@main
+struct iSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
