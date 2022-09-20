024a92614de519e7c4b588bec9b4c7b4a9f91248
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-20 02:50:57 +0200

    Done

--- iSplit/ContentView.swift
+++ iSplit/ContentView.swift
@@ -8,9 +8,95 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var numberOfPeople = 2
+    
+    @State private var amount = 0.0
+    
+    @State private var tipPercentage = 20
+    
+    @FocusState private var isFocused: Bool
+    
+    let tips = [0,10,15,20,25,30]
+    
+    let students = [1,2,3]
+    
+    // MARK: Calculation
+    
+    var totalPerPerson:Double {
+        
+        let peopleCount = Double(numberOfPeople)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValueFractional = amount / 100 * tipSelection
+        let grandTotal = tipValueFractional + amount
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            VStack {
+                
+                Form {
+                    
+                    Section {
+                        
+                        //TextField("Amount: ", value: $amount, format: .currency(code: "EUR"))
+                        
+                        // MARK: Amount Text Field
+                        
+                        TextField("Amount: ", value: $amount, format: .currency(code: Locale.current.currencyCode ?? "EUR"))
+                            .keyboardType(.decimalPad)
+                            .focused($isFocused)
+                        
+                        // MARK: Number of People [picker]
+                        
+                        Picker("Number of People:", selection: $numberOfPeople) {
+                            
+                            ForEach(2..<99, id: \.self) {
+                                
+                                Text("\($0) people")
+                            }
+                        }
+                        
+                    }
+                    
+                    Section {
+                        
+                        // MARK: Tips percentage
+                        
+                        Picker("", selection: $tipPercentage, content: {
+                            
+                            ForEach(tips, id:\.self) {
+                                
+                                Text($0, format: .percent)
+                            }
+                        })
+                        .pickerStyle(.segmented)
+                    } header: {
+                        Text("How much tip do you want to leave?")
+                    }
+                    
+                    Text("Amount per head: $ \(totalPerPerson)")
+                        .font(.title2)
+                        .bold()
+                }
+                .navigationTitle("iSplit")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        
+                        Button("Done") {
+                            isFocused = false
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
